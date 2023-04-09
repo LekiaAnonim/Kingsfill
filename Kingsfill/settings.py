b0ae8f0f189c7a6e7f413ea0334aa99c4ad45e60
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-04-09 20:13:45 +0100

    added og:meta tags

--- Kingsfill/settings.py
+++ Kingsfill/settings.py
@@ -82,15 +82,15 @@ WSGI_APPLICATION = 'Kingsfill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(default='postgresql://postgres:pEE4duWPb7H8Q0pdKFsP@containers-us-west-188.railway.app:7398/railway', conn_max_age=1800),
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     "default": dj_database_url.config(default='postgresql://postgres:pEE4duWPb7H8Q0pdKFsP@containers-us-west-188.railway.app:7398/railway', conn_max_age=1800),
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
